a6d4d8d0304305e149354c02929726bf61d4bd89
suraj rawat <surajrwat001@gmail.com>
2025-07-28 09:14:36 +0530

    update changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url' // ✅ Required to emulate __dirname
+import { dirname } from 'path'      // ✅ Required to emulate __dirname
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
   base: './',
-  plugins: [react(), tailwindcss(),],
-  server: { port: 5173 }
+  plugins: [react(), tailwindcss()],
+  server: { port: 5173 },
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'), // ✅ Correct usage
+    },
+  },
 })

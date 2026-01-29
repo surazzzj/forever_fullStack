0e84e4f7552d3c88d31db6df9e16eef24e54a604
suraj rawat <surajrwat001@gmail.com>
2026-01-29 22:45:09 +0530

    fix vite config and isolate frontend build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/', // ✅ IMPORTANT for deployment
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-  },
   build: {
     outDir: 'dist',
-    sourcemap: false,
   },
 })

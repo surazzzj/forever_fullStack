a6021abfa6448ad4641aa4b7bed05693651c4f80
suraj rawat <surajrwat001@gmail.com>
2026-01-29 21:02:34 +0530

    production url updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
 
-          <a href="https://foreveradminpart.netlify.app">
+        <a href="https://forever-updatedadmin.netlify.app">
           <li className='py-1 px-4 border-gray-200 border-2 rounded-full'>Admin Panel</li>
         </a>
       </ul>
@@ -50,8 +50,8 @@ const Navbar = () => {
           {/* Dropdown Menu */}
           {token && <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
             <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
-              <p onClick={()=>navigate('/profile')} className='cursor-pointer hover:text-black '>My Profile</p>
-              <p onClick={()=>navigate('/orders')} className='cursor-pointer hover:text-black '>Orders</p>
+              <p onClick={() => navigate('/profile')} className='cursor-pointer hover:text-black '>My Profile</p>
+              <p onClick={() => navigate('/orders')} className='cursor-pointer hover:text-black '>Orders</p>
               <p onClick={logout} className='cursor-pointer hover:text-black '>Logout</p>
             </div>
           </div>}
